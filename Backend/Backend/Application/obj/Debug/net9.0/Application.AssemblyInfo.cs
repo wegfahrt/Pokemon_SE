59a953a97994f3ccb8fa2ae43c272201f0fbda08
Wegfahrt <pl.wegfahrt@gmail.com>
2025-04-29 13:42:47 +0200

    updated cors policy

--- Backend/Backend/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Backend/Backend/Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057096e9704128804249280775614a4cff48e9ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cba089513f8d05aafd526de520acf6a96fdf0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/Backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Backend/Backend/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057096e9704128804249280775614a4cff48e9ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cba089513f8d05aafd526de520acf6a96fdf0fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
