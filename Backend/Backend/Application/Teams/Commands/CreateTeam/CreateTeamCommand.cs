90e0e4659f6bc72fd52dceb062185fc8d145e385
Wegfahrt <pl.wegfahrt@gmail.com>
2025-05-02 11:10:00 +0200

    fixed type error

---
+++ Backend/Backend/Application/Teams/Commands/CreateTeam/CreateTeamCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+using Contracts.Teams;
+
+namespace Application.Teams.Commands.CreateTeam;
+
+public record CreateTeamCommand(CreateTeamRequest request) : IRequest<ErrorOr<Success>>;
